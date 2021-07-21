0f482b5a1d65d9d56cf9066125829c73077a18ff
bo-shev <56201331+bo-shev@users.noreply.github.com>
2021-07-21 10:49:55 +0300

    Add files via upload

--- App/Service/Session.php
+++ App/Service/Session.php
@@ -4,10 +4,27 @@ include_once "Users.php";
 
 class Session extends User
 {
+    private $sessionId;
+
+    public function __construct()
+    {
+        $this->start();
+    }
 
     public function setName($name)
     {
-
+        $this->setName($name);
     }
+
+    public function start()
+    {
+        session_start();
+    }
+
+    public function destroy()
+    {
+        session_abort();
+    }
+
 
 }
